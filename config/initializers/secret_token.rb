e8fc7382bd04384c6414c6712df88729e43c17c3
lwalroth <laurie.walroth@gmail.com>
2013-09-19 12:55:15 -0700

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FlaxWebsite::Application.config.secret_key_base = '2b1284ba0d08a9f1e757bd76e0c65c28b2fb136467dc488268f9a869d1457c407da9d3fd752c869eea4012f8980b916d2a63c09586813c80de1128d90080c02b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FlaxWebsite::Application.config.session_store :cookie_store, key: '_flax_website_session'
